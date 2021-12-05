5af1423a98935a6a4fb0b00ea87898de4f1fec88
Lucas Leme Cardoso Ferreira <54031977+LucasLemeCF@users.noreply.github.com>
2021-12-04 23:41:04 -0300

    melhorado o design

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,16 @@
 const alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 function criaTabuleiro() {
-    console.log("Teste");
+    const grama1 = ["verdeClaro", "verdeEscuro"];
+    const grama2 = ["verdeEscuro", "verdeClaro"];
+    var grama = grama1;
+
     var x = document.querySelector("#campo");
     for (let letra = 0; letra < 10; letra++) {
         for (let numero = 1; numero <= 10; numero++) {
-            x.innerHTML += '<div class="q" ' + 'id="' + alfabeto[letra] + numero +'"></div>';
+            x.innerHTML += '<div class="q ' + grama[numero%2] + '" ' + 'id="' + alfabeto[letra] + numero +'"></div>';
         }
+        if (grama == grama1) {grama = grama2} else {grama = grama1};
     }
     
 }
